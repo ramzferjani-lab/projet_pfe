aed52d96589b269ca2f9cd050735488bd941b377
unknown <douha@gmail.com>
2026-03-25 22:57:39 +0100

    first commit

--- app.py
+++ app.py
@@ -2,31 +2,6 @@ from flask import Flask, render_template, request, redirect, url_for, session
 
 app = Flask(__name__)
 app.secret_key = "secret123"
-# DEPARTEMENTS
-
-@app.route('/cms_manuel')
-def cms_manuel():
-    return render_template("cms_manuel.html")
-@app.route('/magasin')
-def magasin():
-    return render_template("magasin.html")
-
-
-
-@app.route('/cms_robotise')
-def cms_robotise():
-    return render_template("cms_robotise.html")
-
-
-@app.route('/controle_test')
-def controle_test():
-    return render_template("controle_test.html")
-
-
-@app.route('/emballage')
-def emballage():
-    return render_template("emballage.html")
-
 
 # comptes autorisés
 users = {
@@ -34,53 +9,59 @@ users = {
     "admin": "admin"
 }
 
+# LOGIN
 @app.route('/')
 def home():
     return redirect(url_for('login'))
 
 @app.route('/login', methods=['GET','POST'])
 def login():
-
     if request.method == 'POST':
-
         username = request.form['username']
         password = request.form['password']
 
         if username in users and users[username] == password:
-
             session['user'] = username
             return redirect(url_for('dashboard'))
-
         else:
             return render_template("login.html", error="Login incorrect")
 
     return render_template("login.html")
 
-
+# DASHBOARD
 @app.route('/dashboard')
 def dashboard():
-
     if 'user' in session:
         return render_template("dashboard.html")
-
     return redirect(url_for('login'))
 
+# DEPARTEMENTS
+@app.route('/cms_manuel')
+def cms_manuel():
+    return render_template("cms_manuel.html")
 
-@app.route('/cmsmanuel')
-def cmsmanuel():
+@app.route('/cms_robotise')
+def cms_robotise():
+    return render_template("cms_robotise.html")
 
-    if 'user' in session:
-        return render_template("cms_manuel.html")
+@app.route('/controle_test')
+def controle_test():
+    return render_template("controle_test.html")
 
-    return redirect(url_for('login'))
+@app.route('/emballage')
+def emballage():
+    return render_template("emballage.html")
 
+@app.route('/magasin')
+def magasin():
+    return render_template("magasin.html")
 
+# LOGOUT
 @app.route('/logout')
 def logout():
-
-    session.pop('user',None)
+    session.pop('user', None)
     return redirect(url_for('login'))
 
-
-if __name__ == '__main__':
-    app.run(debug=True)
+# RUN (مرة وحدة فقط)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
